32084866f9c00e42aa1fa43dc06472fe757b0b74
Pedro Liberal <phguiliberal+tech@gmail.com>
2026-04-05 22:09:17 -0300

    Updated monitor scripts

--- monitor144mhz.sh
+++ monitor144mhz.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
 kscreen-doctor output.1.mode.1
+#sleep 0.2
+flatpak kill org.ferdium.Ferdium || true
+flatpak run org.ferdium.Ferdium --hidden &
--- monitor60mhz.sh
+++ monitor60mhz.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
-# 1. Altera para 60Hz (ou 144Hz no outro script)
 kscreen-doctor output.1.mode.2
-
-# 2. Pequeno delay para o KWin e o driver de vídeo estabilizarem
-sleep 1.5
-
-# 3. Fecha o Ferdium se estiver aberto (o '|| true' evita mensagens de erro)
+#sleep 0.2
 flatpak kill org.ferdium.Ferdium || true
-
-# 4. Abre o Ferdium novamente em background e minimizado
 flatpak run org.ferdium.Ferdium --hidden &
